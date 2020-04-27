aaede4089b7e2e6325c6536ea6996a64c408bf3d
anoorzi <anoorzi123@gmail.com>
2020-04-27 11:43:01 -0400

    Yesir

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,46 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis:[],
+    cash: 200,
+    cost: 0
+  }
+
+  handleRender = () => {
+    fetch(`http://localhost:3000/sushis`)
+    .then(resp => resp.json())
+    .then(data =>{
+      this.setState({
+        sushis: data
+      })
+    })
+  }
+
+  handleMore = () =>{
+    let added = this.state.slideIndex + 4
+    this.setState({
+      slideIndex:added
+    })
+  }
+
+  addCost = (price) =>{
+    if(this.state.cash >= this.state.cost + price){
+      let total = this.state.cost + price
+      this.setState({
+        cost:total
+      })
+    }
+
+
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <button onClick = {this.handleRender}> get Sushis</button>
+        <SushiContainer addCost = {this.addCost}sushis = {this.state.sushis} handleRender={this.handleRender}/>
+        <Table cash = {this.state.cash} cost = {this.state.cost}/>
       </div>
     );
   }
